e8bf2b9f68045bf84f39b32368e1cad9b712df9a
Lisong Xu <xu@unl.edu>
2023-08-17 17:10:04 -0500

    Update StatisticsDemo.java

--- src/main/java/unl/soc/StatisticsDemo.java
+++ src/main/java/unl/soc/StatisticsDemo.java
@@ -30,7 +30,13 @@ public class StatisticsDemo {
 		s.close();
 
 		int min = Statistics.getMin(array);
+		int max = Statistics.getMax(array);
+		int sum = Statistics.getSum(array);
+		double ave = Statistics.getAverage(array);
 
+		System.out.println("The sum is " + sum);
+		System.out.println("The average is " + ave);
+		System.out.println("The highest is " + max);
 		System.out.println("The lowest is " + min);
 
 	}
